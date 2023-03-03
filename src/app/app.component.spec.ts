9cef54c8e4752395e4b10c3a9d1bf718163861d6
Tony-Mok <tonymok97@gmail.com>
2023-03-02 23:50:15 -0500

    angular init 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hku-project'`, () => {
+  it(`should have as title 'myApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hku-project');
+    expect(app.title).toEqual('myApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hku-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myApp app is running!');
   });
 });
